39bb118e8ad59322175ffd98736da4d9ea3230bf
caducoder <cadudrop@hotmail.com>
2024-01-18 18:42:27 -0300

    feat: add navbar and mobile navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Sidebar from './_components/sidebar'
+import Navbar from './_components/navbar'
 
-const DashboardLayout = ({children}: {children: React.ReactNode}) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-full'>
-        <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
-            <Sidebar />
+    <div className='h-full flex flex-row'>
+      <div className='hidden md:flex h-full w-56 flex-col inset-y-0 z-50'>
+        <Sidebar />
+      </div>
+      <main className='w-full'>
+        <div className='h-[80px] w-full z-50 inset-y-0'>
+          <Navbar />
         </div>
         {children}
+      </main>
     </div>
   )
 }
