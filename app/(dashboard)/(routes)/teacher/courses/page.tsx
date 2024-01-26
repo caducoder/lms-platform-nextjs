d7e8b99073c4335885cea0d946ccca53105723f0
caducoder <cadudrop@hotmail.com>
2024-01-26 19:49:52 -0300

    feat: add courses table view

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,35 @@
 import { Button } from '@/components/ui/button'
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const CoursesPage = () => {
+import { columns } from './_components/columns'
+import { DataTable } from './_components/data-table'
+
+const CoursesPage = async () => {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect("/")
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+
   return (
     <div className='p-6'>
       <Link href="/teacher/create">
         <Button>Novo Curso</Button>
       </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   )
 }
