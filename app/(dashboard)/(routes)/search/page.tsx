e2e54f387f4c31e8c4f458f347053d7e9416630b
Carlos Americo <carlosamericodev@gmail.com>
2024-01-27 15:45:03 -0300

    feat: add search input

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,6 +1,7 @@
 import { db } from '@/lib/db'
 import React from 'react'
 import Categories from "./_components/categories"
+import SearchInput from "@/components/search-input"
 
 const SearchPage = async () => {
   const categories = await db.category.findMany({
@@ -11,11 +12,16 @@ const SearchPage = async () => {
   
 
   return (
-    <div className="p-6">
-      <Categories
-        items={categories}
-      />
+    <>
+    <div className="px-6 pt-6 md:hidden md:mb-0 block">
+      <SearchInput />
     </div>
+      <div className="p-6">
+        <Categories
+          items={categories}
+        />
+      </div>
+    </>
   )
 }
 
